b1421d04e54a38635c62e78165f5593eae0fcf51
Shohruhmirzo Ismoilov <ismoilovshohruhmirzo28@gmail.com>
2026-04-17 11:28:37 +0500

    Fix media files in production

--- wco_conference/urls.py
+++ wco_conference/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', include('conference.urls', namespace='conference')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
